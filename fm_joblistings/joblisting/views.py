be185224c63b089f55b39712fd3d257b808607dc
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-27 19:07:14 +0000

    set up basic index route with joblisting context

--- fm_joblistings/joblisting/views.py
+++ fm_joblistings/joblisting/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def index(request):
+    joblistings = Job.objects.all()
+    return render(
+        request,
+        'joblisting/index.html',
+        {"joblistings": joblistings}
+    )
