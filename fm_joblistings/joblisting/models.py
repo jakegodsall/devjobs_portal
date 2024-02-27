34a546465460f55a3c07f2929609fb84c4c291ae
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-27 18:56:53 +0000

    removed sqlite from repo

--- fm_joblistings/joblisting/models.py
+++ fm_joblistings/joblisting/models.py
@@ -8,53 +8,6 @@ class Company(models.Model):
         return self.name
     
 
-class Level(models.Model):
-    LEVELS = {
-        "J": "JUNIOR",
-        "M": "MIDWEIGHT",
-        "S": "SENIOR"
-    }
-
-    level = models.CharField(
-        max_length=1,
-        choices=LEVELS,
-        default="MIDWEIGHT")
-    
-    def __str__(self):
-        return self.LEVELS[self.level]
-    
-
-class Contract(models.Model):
-    CONTRACT_TYPES = {
-        "PT": "Part Time",
-        "FT": "Full Time"
-    }
-
-    contract = models.CharField(
-        max_length=2,
-        choices=CONTRACT_TYPES,
-        default="FT")
-    
-    def __str__(self):
-        return self.CONTRACT_TYPES[self.contract]
-    
-    
-class Role(models.Model):
-    ROLES = {
-        "FE": "Frontend",
-        "FS": "Fullstack",
-        "BE": "Backend"
-    }
-
-    role = models.CharField(
-        max_length=2,
-        choices=ROLES,
-        default="FS")
-    
-    def __str__(self):
-        return self.ROLES[self.role]
-    
-    
 class Language(models.Model):
     name = models.CharField(max_length=100)
 
@@ -69,11 +22,25 @@ class Tool(models.Model):
     
 
 class Job(models.Model):
+    class Role(models.TextChoices):
+        FRONTEND = "FE", "Frontend"
+        FULLSTACK = "FS", "Fullstack"
+        BACKEND = "BE", "Backend"
+
+    class Level(models.TextChoices):
+        JUNIOR = "J", "Junior"
+        MIDWEIGHT = "M", "Midweight"
+        SENIOR = "S", "Senior"
+    
+    class Contract(models.TextChoices):
+        PART_TIME = "PT", "Part Time"
+        FULL_TIME = "FT", "Full Time"
+    
     position = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
-    contract = models.CharField(Contract, on_delete=models.CASCADE)
+    role = models.CharField(max_length=2, choices=Role.choices)    
+    level = models.Charfield(max_length=1, choices=Level.choices, default=Level.JUNIOR)
+    contract = models.CharField(max_length=2, choices=Contract.choices, default=Contract.FULL_TIME)
     posted_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     is_featured = models.BooleanField(default=False)
