c63cec4fe17f4e34f9bac06e785f9f5da2b96240
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-28 19:19:50 +0000

    added file field to company for logo, bootstrapped data

--- fm_joblistings/fm_joblistings/settings.py
+++ fm_joblistings/fm_joblistings/settings.py
@@ -117,11 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
--- fm_joblistings/joblisting/models.py
+++ fm_joblistings/joblisting/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
+    logo = models.FileField(upload_to='logos/', null=True)
     
     def __str__(self):
         return self.name
