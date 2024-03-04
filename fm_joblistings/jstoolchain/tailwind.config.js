319a22cb318c48168d0c3a9dab9c4328eb489f59
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-04 19:11:20 +0000

    extracted base.html and tailwind config to project dir to allow for multiple apps

--- fm_joblistings/jstoolchain/tailwind.config.js
+++ fm_joblistings/jstoolchain/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../templates/**/*.html"],
+  content: ["../templates/**/*.html", "../joblisting/templates/**/*.html"],
   theme: {
     extend: {
       colors: {
