e6662dfb623be056a93c5670aeb14500913b0505
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-04 09:11:22 +0000

    rendered languages and tools in the job_item template

--- fm_joblistings/joblisting/views.py
+++ fm_joblistings/joblisting/views.py
@@ -3,7 +3,8 @@ from .models import Job
 
 # Create your views here.
 def index(request):
-    joblistings = Job.objects.all()
+    joblistings = Job.objects.all().prefetch_related('languages').prefetch_related('tools')
+    print(joblistings.get(pk=1))
     return render(
         request,
         'joblisting/index.html',
