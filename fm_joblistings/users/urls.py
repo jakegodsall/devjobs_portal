67168381d056380883da4e44e5f1bc5a1be1417b
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-11 13:54:27 +0100

    added a basic navigation bar for navigating to auth endpoints

--- fm_joblistings/users/urls.py
+++ fm_joblistings/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import register_user, register_client, register_company
+from .views import login_user, register_user, register_client, register_company, profile
 
 app_name = "users"
 urlpatterns = [
+    path("login/", login_user, name="login"),
     path("register_user/", register_user, name="register_user"),
     path("register_client/", register_client, name="register_client"),
-    path("register_company/", register_company, name="register_company")
+    path("register_company/", register_company, name="register_company"),
+    path("profile/", profile, name="profile")
 ]
--- fm_joblistings/users/views.py
+++ fm_joblistings/users/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
 from .forms import UserProfileForm, ClientForm, CompanyForm
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("users:profile")
+    form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+        
 
 # Create your views here.
 def register_user(request):
@@ -30,3 +45,6 @@ def register_client(request):
         ...
     client_form = ClientForm()
     return render(request, "users/register_client.html", { "client_form":  client_form})
+
+def profile(request):
+    return "Hello"
