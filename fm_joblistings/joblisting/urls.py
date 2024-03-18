adbe4c01cea7c537b5ccc02800e3cdbbad3f65df
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-18 15:38:29 +0100

    added class-based view to create job endpoint

--- fm_joblistings/joblisting/urls.py
+++ fm_joblistings/joblisting/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, create_job
+from .views import index, CreateJobView
 
 app_name = "jobs"
 urlpatterns = [
     path('', index, name="index"),
-    path('create/', create_job, name="create_job")
+    path('create/', CreateJobView.as_view(), name="create_job")
 ]
--- fm_joblistings/joblisting/views.py
+++ fm_joblistings/joblisting/views.py
@@ -1,6 +1,9 @@
+from django.views import View
 from django.shortcuts import render, redirect
+
 from .models import Job
 from .forms import JobForm
+
 
 # Create your views here.
 def index(request):
@@ -12,14 +15,17 @@ def index(request):
         {"joblistings": joblistings}
     )
 
-def create_job(request):
-    if request.method == "POST":
-        form = JobForm(request.POST)
+class CreateJobView(View):
+    form_class = JobForm
+    template = "joblisting/create.html"
 
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template, {"form": form})
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
-
             return redirect("jobs:index")
-
-    form = JobForm()
-    return render(request, "joblisting/create.html", {"form": form})
+        return render(request, self.template, {"form": form})
