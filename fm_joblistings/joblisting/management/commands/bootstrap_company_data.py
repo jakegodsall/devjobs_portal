3f7b2666fbae7b57b63109d0d47e974422f22e33
Jake Godsall <91415918+jakegodsall@users.noreply.github.com>
2024-04-03 08:49:40 +0100

    added shell for management commands for bootstrapping company and job data to the application via JSON

---
+++ fm_joblistings/joblisting/management/commands/bootstrap_company_data.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+from django.core.management.base import BaseCommand
+
+from joblisting.models import Company, Job
+
+WORKING_DIR = Path('.')
+
+class Command(BaseCommand):
+    help = 'Bootstrap company data from JSON to the database'
+
+    def handle(self, *args, **kwargs):
+        with open(WORKING_DIR / 'bootstrap_data' / 'company_data.json') as file:
+            data = json.load(file)
+            for entry in data:
+                Company.objects.create(
+
+                )
---
+++ fm_joblistings/joblisting/management/commands/bootstrap_job_data.json.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+from django.core.management.base import BaseCommand
+
+from joblisting.models import Company, Job
+
+WORKING_DIR = Path('.')
+
+
+class Command(BaseCommand):
+    help = 'Bootstrap job data from JSON to the database'
+
+    def handle(self, *args, **kwargs):
+        with open(WORKING_DIR / 'bootstrap_data' / 'job_data.json') as file:
+            data = json.load(file)
+            for job in data:
+                Job.objects.create(
+                    position=job['position'],
+                    role=job['role'],
+                    level=job['level'],
+                    posted_at=
+                )
