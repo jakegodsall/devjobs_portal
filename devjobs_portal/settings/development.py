8d85056d20fb46c6cd29b9f4f303b0be10d069fb
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-10 13:10:23 +0100

    set up dev environment to use a postgres db inside docker container

--- devjobs_portal/settings/development.py
+++ devjobs_portal/settings/development.py
@@ -5,8 +5,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'),
+        'PORT': os.getenv('DATABASE_PORT', 5432),
     }
 }
 
