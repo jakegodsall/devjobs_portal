22e3a1c34cb459110c4c37700931b32f07ae16d2
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-10 13:42:48 +0100

    added production db config

--- devjobs_portal/settings/development.py
+++ devjobs_portal/settings/development.py
@@ -2,7 +2,6 @@ from .base import *
 
 DEBUG = True
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
--- devjobs_portal/settings/production.py
+++ devjobs_portal/settings/production.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .base import *
 
 DEBUG = False
@@ -7,15 +8,21 @@ INSTALLED_APPS += [
     'storages'
 ]
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
+
 # AWS S3 Credentials
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')  # Adjust if needed
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-north-1')
 
 # S3 settings
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = None  # Ensure the files are public by default or manage permissions in S3
+AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',  # Cache files for 1 day
 }
