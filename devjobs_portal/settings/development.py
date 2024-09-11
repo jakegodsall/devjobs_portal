339980fadb347220cdf7f4422b2b5ea7b046fd5a
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-11 19:41:34 +0100

    added handling of static and media files in debug mode

--- devjobs_portal/settings/development.py
+++ devjobs_portal/settings/development.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 DATABASES = {
     'default': {
--- job_portal/urls.py
+++ job_portal/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import index, CreateJobView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = "jobs"
 urlpatterns = [
     path('', index, name="index"),
     path('create', CreateJobView.as_view(), name="create_job")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
