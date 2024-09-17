3f9f23f62250a2a0962f55d80cdadaed07972db9
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-17 18:08:21 +0100

    fixed static location

--- devjobs_portal/storages.py
+++ devjobs_portal/storages.py
@@ -2,7 +2,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = "staticfiles"
+    location = "static"
 
 
 class MediaStorage(S3Boto3Storage):
